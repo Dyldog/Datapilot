1bb52230528a957ed8f246bb5baaf0cc53f4de32
Dylan Elliott <dylan.elliott@gmail.com>
2024-07-18 00:34:27 +1000

    Fix content view not showping

--- Datapilot/DatapilotApp.swift
+++ Datapilot/DatapilotApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DatapilotApp: App {
     var body: some Scene {
         WindowGroup {
-            NavigationView {
+            NavigationStack {
                 QueryList()
 					.navigationTitle(titles.randomElement()!)
             }
